01aebb4b5f1f5cb5f3450dc7cc2823b2f1b273c0
benboesel <beboes@microsoft.com>
2019-04-19 02:26:14 -0700

    Started to explore basic grab/drop into cell interaction

---
+++ TicTac/Assets/TicTacToe/Scripts/Cell.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    
+    public enum State
+    {
+        Empty,
+        X,
+        O
+    }
+
+    public enum HoveredStates
+    {
+        Unhovered,
+        Hovered,
+        Filled
+    }
+
+    private State state;
+
+    private List<GamePiece> hoveringPieces = new List<GamePiece>();
+
+    public State GetState()
+    {
+        return state;
+    }
+
+    public bool IsEmpty()
+    {
+        return state == State.Empty;
+    }
+
+    public void Clear()
+    {
+        state = State.Empty;
+        //Delete gamepiece inside of itself
+    }
+
+    public void OnPieceEnter(GamePiece piece)
+    {
+        if (hoveringPieces.Contains(piece))
+            return;
+
+        hoveringPieces.Add(piece);
+        piece.OnReleased += OnGamePieceReleased;
+        if (IsEmpty())
+        {
+            SetHighlightState(HoveredStates.Hovered);
+        }
+        else
+        {
+            SetHighlightState(HoveredStates.Filled);
+        }
+    }
+    
+    public void OnPieceExit(GamePiece piece)
+    {
+        if (!hoveringPieces.Contains(piece))
+            return;
+        
+        piece.OnReleased -= OnGamePieceReleased;
+        hoveringPieces.Remove(piece);
+        if (hoveringPieces.Count == 0)
+        {
+            SetHighlightState(HoveredStates.Unhovered);
+        }
+    }
+
+    private void OnGamePieceReleased(GamePiece gamePiece)
+    {
+        if(gamePiece.type == GamePiece.Type.O)
+        {
+            SetCellState(State.O);
+        }
+        else
+        {
+            SetCellState(State.X);
+        }
+    }
+
+    private void SetCellState(State state)
+    {
+        this.state = state;
+    }
+
+    private void SetHighlightState(HoveredStates highlightState)
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if(highlightState == HoveredStates.Unhovered)
+        {
+            meshRenderer.material.color = Color.white;
+        }
+        if (highlightState == HoveredStates.Hovered)
+        {
+
+            meshRenderer.material.color = Color.green;
+        }
+        if(highlightState == HoveredStates.Filled)
+        {
+            meshRenderer.material.color = Color.red;
+        }
+    }
+
+}
---
+++ TicTac/Assets/TicTacToe/Scripts/GamePiece.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class GamePiece : OVRGrabbable
+{
+    public enum Type
+    {
+        X,
+        O
+    }
+
+    public Type type;
+
+    public delegate void GrabAction(GamePiece gamePiece);
+    public GrabAction OnReleased;
+    private List<Cell> hoveringCells = new List<Cell>();
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(linearVelocity, angularVelocity);
+        if(OnReleased != null)
+        {
+            OnReleased.Invoke(this);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Cell cell = other.GetComponent<Cell>();
+        if (cell != null && hoveringCells.Contains(cell))
+        {
+            cell.OnPieceExit(this);
+            hoveringCells.Remove(cell);
+            if(hoveringCells.Count > 0)
+            {
+                hoveringCells[hoveringCells.Count -1 ].OnPieceEnter(this);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Cell cell = other.GetComponent<Cell>();
+        if (cell != null && !hoveringCells.Contains(cell))
+        {
+            if(hoveringCells.Count > 0)
+            {
+                hoveringCells[hoveringCells.Count - 1].OnPieceExit(this);
+            }
+            cell.OnPieceEnter(this);
+            hoveringCells.Add(cell);
+        }
+    }
+
+}
